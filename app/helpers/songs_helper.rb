684635f978525f5f99616f95bfab4571942cb332
Wasif Khan <waasif.khaan@gmail.com>
2018-10-16 20:09:03 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,11 @@
 module SongsHelper
 
+  def artist_id_field(song)
+    if song.artist.nil?
+      select_tag "song[artist_id]", options_from_collection_for_select(Artitst.all, :id, :name)
+    else
+      hidden_field_tag "song[artist_id]", sont.artist_id
+    end
+  end
+
 end
