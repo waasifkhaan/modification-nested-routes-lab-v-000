831463c347746a87cf6a4bb0d005a7b29fa74f41
Wasif Khan <waasif.khaan@gmail.com>
2018-10-16 22:04:58 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -46,7 +46,7 @@ class SongsController < ApplicationController
     if Artist.find_by(id: params[:artist_id])
       @song = Song.find_by(id: params[:id])
     else
-      redirect_to artists_path(@artist) , alert: "Artist no found"
+      redirect_to artists_path(@artist) , alert: "Artist not found"
     end
 
   end
