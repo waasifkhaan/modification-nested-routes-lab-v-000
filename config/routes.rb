a37bf81a60972ec5ee2a9a5a04fa67dde86e2deb
Wasif Khan <waasif.khaan@gmail.com>
2018-10-16 20:04:28 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  resources :artists do
-    resources :songs, only: [:index, :show]
-  end
-  resources :songs
-
   resources :artists, only: [:show, :index] do
   resources :songs, only: [:show, :index, :new, :edit]
-end
+  end
+
+    resources :songs
 end
