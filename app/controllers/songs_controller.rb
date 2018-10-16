638111ddebaca1db5db9ae0d3f7b26d8cdbc0b5e
Wasif Khan <waasif.khaan@gmail.com>
2018-10-16 20:03:14 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -64,6 +64,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_id)
   end
 end
